751cab365de36144c4ac9e748a02e22c5b713027
akiya kudo <akiyastyle@gmail.com>
2025-04-28 21:28:50 +0900

    Create .asmdef files

---
+++ Assets/Tests/Runtime/RuntimeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+[Description("RuntimeTests")]
+public class RuntimeTests
+{
+    [UnityTest]
+    public IEnumerator Test()
+    {
+        Assert.AreEqual(1, 1);
+        yield return null;
+    }
+}
